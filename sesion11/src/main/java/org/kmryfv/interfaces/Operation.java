80f675f040f8b69f5ab8e16059e1c2f5995547a2
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2024-09-24 18:53:15 -0600

    First commit

---
+++ sesion11/src/main/java/org/kmryfv/interfaces/Operation.java
@@ -0,0 +1,12 @@
+package org.kmryfv.interfaces;
+
+import java.util.ArrayList;
+
+public interface Operation {
+    /*CRUD*/
+    public void create(Object object);
+    public ArrayList<Object> read();
+    public void update(Object object);
+    public void delete(String id);
+    public Object search(String value);
+}
